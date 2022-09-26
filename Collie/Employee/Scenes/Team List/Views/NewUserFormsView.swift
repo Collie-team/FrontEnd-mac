85416a5133d33e802b449d992a2cb1f3d15b791a
Pablo <harbar2705@gmail.com>
2022-09-26 18:46:43 -0300

    alignment fail

---
+++ Collie/Employee/Scenes/Team List/Views/NewUserFormsView.swift
@@ -0,0 +1,8 @@
+//
+//  NewUserFormsView.swift
+//  Collie
+//
+//  Created by Pablo Penas on 26/09/22.
+//
+
+import Foundation
---
+++ Collie/Employee/Scenes/Team List/Views/ProgressBarView.swift
@@ -0,0 +1,20 @@
+//
+//  ProgressBarView.swift
+//  Collie
+//
+//  Created by Pablo Penas on 26/09/22.
+//
+
+import SwiftUI
+
+struct ProgressBarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ProgressBarView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProgressBarView()
+    }
+}
