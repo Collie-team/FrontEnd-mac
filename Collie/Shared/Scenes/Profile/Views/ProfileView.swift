f8a84c50529cb91a4e3b0ed599bf0f9d168cd688
Pablo <harbar2705@gmail.com>
2022-11-11 15:32:34 -0300

    profile view frontend

--- Collie/Shared/Scenes/Profile/Views/ProfileView.swift
+++ Collie/Shared/Scenes/Profile/Views/ProfileView.swift
@@ -8,8 +8,9 @@
 import SwiftUI
 
 struct ProfileView: View {
+    @State var editingMode: Bool = false
     var body: some View {
-        VStack {
+        VStack(spacing: 20) {
             HStack {
                 Text("Perfil")
                     .font(.system(size: 40, weight: .bold, design: .default))
@@ -26,6 +27,7 @@ struct ProfileView: View {
                         .font(.system(size: 15))
                 }
                 .frame(maxWidth: 315)
+                Spacer()
                 VStack(alignment: .leading) {
                     Text("Foto de Perfil")
                     RoundedRectangle(cornerRadius: 8)
@@ -34,97 +36,24 @@ struct ProfileView: View {
                         .overlay(
                             ZStack {
                                 Image(systemName: "camera.fill")
-                                    .foregroundColor(Color.collieLilas)
-                                    .overlay(
-                                        RoundedRectangle(cornerRadius: 8)
-                                            .stroke(Color.collieTextFieldBorder, lineWidth: 1)
-                                    )
+                                    .foregroundColor(Color.collieRoxo)
+                                    .font(.system(size: 28))
                             }
-                                .frame(width: 60, height: 60)
-                                .background(Color.white)
-                                .cornerRadius(8)
+                            .frame(width: 60, height: 60)
+                            .background(Color.white)
+                            .cornerRadius(8)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 8)
+                                    .stroke(Color.collieTextFieldBorder, lineWidth: 1)
+                            )
                             .offset(x: 100, y: 100)
                         )
                 }
                 Spacer()
-                VStack {
-                    HStack {
-                        Spacer()
-                        Button(action: {}) {
-                            Text("Editar \(Image(systemName: "square.and.pencil"))")
-                                .foregroundColor(Color.collieRoxo)
-                                .font(.system(size: 20))
-                        }
-                        .buttonStyle(.plain)
-                    }
-                    VStack {
-                        VStack(alignment: .leading) {
-                            Text("Nome do usuário")
-                                .font(.system(size: 15))
-                            HStack {
-                                Text("Laura Gomes")
-                                    .font(.system(size: 22, weight: .bold))
-                                Spacer()
-                            }
-                            Divider()
-//                            Rectangle()
-//                                .fill(Color.collieTextFieldBorder)
-//                                .frame(maxWidth: .infinity, maxHeight: 1)
-                        }
-                        VStack(alignment: .leading) {
-                            
-                            Text("Cargo")
-                                .font(.system(size: 15))
-                            HStack {
-                                Text("Sem cargo")
-                                    .font(.system(size: 17, weight: .bold))
-                                Spacer()
-                            }
-                            Divider()
-//                            Rectangle()
-//                                .fill(Color.collieTextFieldBorder)
-//                                .frame(maxWidth: .infinity, maxHeight: 1)
-                        }
-                        
-                        
-                        VStack(alignment: .leading) {
-                            
-                            Text("Breve descrição")
-                                .font(.system(size: 15))
-                            HStack {
-                                Text("Sem descrição")
-                                    .font(.system(size: 17, weight: .bold))
-                                Spacer()
-                            }
-                            Divider()
-//                            Rectangle()
-//                                .fill(Color.collieTextFieldBorder)
-//                                .frame(maxWidth: .infinity, maxHeight: 1)
-                        }
-                        
-                        VStack(alignment: .leading) {
-                            Text("Data de entrada")
-                                .font(.system(size: 15))
-                            HStack {
-                                Text("17/04/2022")
-                                    .font(.system(size: 17))
-                                Spacer()
-                            }
-                            Divider()
-//                            Rectangle()
-//                                .fill(Color.collieTextFieldBorder)
-//                                .frame(maxWidth: .infinity, maxHeight: 1)
-                            
-                            Text("Papel")
-                                .font(.system(size: 15))
-                            HStack {
-                                Text("Colaborador")
-                                    .font(.system(size: 17))
-                                Spacer()
-                            }
-                        }
-                    }
-                    .frame(maxWidth: 600)
+                if editingMode {
+                    editingForm
+                } else {
+                    displayForm
                 }
             }
             .frame(maxWidth: .infinity)
@@ -156,10 +85,11 @@ struct ProfileView: View {
                             )
                     }
                     .buttonStyle(.plain)
+                    .contentShape(Rectangle())
                 }
             }
             .frame(maxWidth: .infinity)
-            .padding([.top, .horizontal], 32)
+            .padding(32)
             .background(Color.white)
             .cornerRadius(12)
             
@@ -184,12 +114,15 @@ struct ProfileView: View {
                         )
                 }
                 .buttonStyle(.plain)
+                .contentShape(Rectangle())
             }
             .frame(maxWidth: .infinity)
-            .padding([.top, .horizontal], 32)
+            .padding(32)
             .background(Color.white)
             .cornerRadius(12)
             
+            
+            Spacer()
         }
         .padding(.horizontal, 60)
         .padding(.vertical, 32)
@@ -206,6 +139,154 @@ struct ProfileView: View {
         //                }
         //            )
         //        }
+    }
+    
+    var displayForm: some View {
+        VStack {
+            HStack {
+                Spacer()
+                Button(action: {
+                    editingMode = true
+                }) {
+                    Text("Editar \(Image(systemName: "square.and.pencil"))")
+                        .foregroundColor(Color.collieRoxo)
+                        .font(.system(size: 20))
+                }
+                .buttonStyle(.plain)
+                .contentShape(Rectangle())
+            }
+            VStack {
+                VStack(alignment: .leading) {
+                    Text("Nome do usuário")
+                        .font(.system(size: 15))
+                    HStack {
+                        Text("Laura Gomes")
+                            .font(.system(size: 22, weight: .bold))
+                        Spacer()
+                    }
+                    Divider()
+                }
+                VStack(alignment: .leading) {
+                    
+                    Text("Cargo")
+                        .font(.system(size: 15))
+                    HStack {
+                        Text("Sem cargo")
+                            .font(.system(size: 17, weight: .bold))
+                        Spacer()
+                    }
+                    Divider()
+                }
+                
+                VStack(alignment: .leading) {
+                    Text("Breve descrição")
+                        .font(.system(size: 15))
+                    HStack {
+                        Text("Sem descrição")
+                            .font(.system(size: 17, weight: .bold))
+                        Spacer()
+                    }
+                    Divider()
+                }
+                
+                VStack(alignment: .leading) {
+                    Text("Data de entrada")
+                        .font(.system(size: 15))
+                    HStack {
+                        Text("17/04/2022")
+                            .font(.system(size: 17))
+                        Spacer()
+                    }
+                    Divider()
+                    Text("Papel")
+                        .font(.system(size: 15))
+                    HStack {
+                        Text("Colaborador")
+                            .font(.system(size: 17))
+                        Spacer()
+                    }
+                }
+                .padding(.bottom, 60)
+            }
+            .frame(maxWidth: 600)
+        }
+    }
+    
+    var editingForm: some View {
+        VStack {
+            HStack {
+                Spacer()
+            }
+            VStack {
+                VStack(alignment: .leading) {
+                    Text("Nome do usuário")
+                        .font(.system(size: 15))
+                    CustomTextField("Nome", text: .constant("Laura Gomes"))
+                    Divider()
+                }
+                VStack(alignment: .leading) {
+                    
+                    Text("Cargo")
+                        .font(.system(size: 15))
+                    CustomTextField("Cargo", text: .constant("Sem cargo"))
+                    Divider()
+                }
+                
+                VStack(alignment: .leading) {
+                    Text("Breve descrição")
+                        .font(.system(size: 15))
+                    CustomTextField("Description", text: .constant("Sem descrição"))
+                    Divider()
+                }
+                
+                VStack(alignment: .leading) {
+                    Text("Data de entrada")
+                        .font(.system(size: 15))
+                    HStack {
+                        Text("17/04/2022")
+                            .font(.system(size: 17))
+                        Spacer()
+                    }
+                    Divider()
+                    Text("Papel")
+                        .font(.system(size: 15))
+                    HStack {
+                        Text("Colaborador")
+                            .font(.system(size: 17))
+                        Spacer()
+                    }
+                    HStack {
+                        Spacer()
+                        Button(action: {
+                            editingMode = false
+                        }) {
+                            Text("Reverter alterações")
+                                .padding(.horizontal, 24)
+                                .padding(.vertical, 12)
+                        }
+                        .buttonStyle(.plain)
+                        .contentShape(Rectangle())
+                        .background(.white)
+                        .overlay(RoundedRectangle(cornerRadius: 8).stroke(Color.gray, lineWidth: 1))
+                        
+                        Button(action: {
+                            editingMode = false
+                        }) {
+                            Text("Salvar alterações")
+                                .padding(.horizontal, 24)
+                                .padding(.vertical, 12)
+                        }
+                        .buttonStyle(.plain)
+                        .contentShape(Rectangle())
+                        .background(.white)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 8).stroke(Color.gray, lineWidth: 1))
+                    }
+                }
+                .padding(.bottom, 60)
+            }
+            .frame(maxWidth: 600)
+        }
     }
 }
 
