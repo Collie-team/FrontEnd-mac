070da7c90b2a30a2ecf3ddd994f7abbe9ba0467c
André Arns <andreluisarns@gmail.com>
2022-11-23 14:56:09 -0300

    Onboarding user default keys updated

--- Collie/Shared/Services/UserDefault/OnbardingStorageService.swift
+++ Collie/Shared/Services/UserDefault/OnbardingStorageService.swift
@@ -6,12 +6,12 @@ final class OnboardingStorageService {
     private let userDefaultsService = UserDefaultService()
     
     func markOnboardingDone(onboardingType: OnboardingType) {
-        let key = "Is\(onboardingType.rawValue)OnboardingDone2"
+        let key = "Is\(onboardingType.rawValue)OnboardingDone"
         userDefaultsService.setBoolForKey(value: true, key: key)
     }
     
     func isOnboardingDone(onboardingType: OnboardingType) -> Bool {
-        let key = "Is\(onboardingType.rawValue)OnboardingDone2"
+        let key = "Is\(onboardingType.rawValue)OnboardingDone"
         return userDefaultsService.readBoolForKey(key: key)
     }
 }
